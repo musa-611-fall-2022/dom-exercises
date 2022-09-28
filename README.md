02f0a2dc4e33e6591f353e1a954c414251b5effa
Mjumbe Poe <mjumbewu@gmail.com>
2022-09-27 23:00:43 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Practice with DOM manipulation
+
+1. Fork this repository
+2. Go to **Settings** in your repository and select **Actions > General**
+3. Select the **Allow all actions and reusable workflows** option and click **Save**
+4. Go to the **Actions** tab and click the green button if actions are not yet enabled
+
+The folder under _exercises/_ contains an HTML file and a JavaScript file called _index.js_. Start a web server (using `npx http-server`), open the HTML file (which will be at http://127.0.0.1:8080/exercises/part1-selectors-and-events/) in your browser, and work through the exercises as instructed in the JavaScript file. The .js file gives instructions for what you should see in the console if you're on the right track. You should also see "Success!" five times on the page.
+
+To run all the tests, open one terminal and start a web server:
+
+```bash
+npx http-server
+```
+
+Then, from another terminal, run the tests:
+
+```bash
+npm test
+```
+
+You can also lint your code with the following command:
+
+```bash
+npx eslint .
+```
+
+When you push your code to GitHub, GitHub _should_ also run the same set of tests and lints.
---
+++ exercises/part1-selectors-and-events/index.js
@@ -0,0 +1,122 @@
+/* =====================
+
+# Exercise: Selectors & Events
+
+## Introduction
+
+Open the index.html page in your browser, and update the variables and functions
+below so that the results all read "Success!".
+
+===================== */
+
+import { htmlToElement } from './template-tools.js';
+
+/* ====================
+Part 1: Set the variable below equal to the paragraph element representing the
+first test result.
+==================== */
+
+let firstResult;
+
+/* ====================
+Parts 2: Set the variable below equal to a collection of the paragraph
+elements representing the 2nd and 3rd results.
+==================== */
+
+let secondAndThirdResults;
+
+/* ====================
+Parts 3: Set the variable below equal to a collection of the paragraph
+elements representing the all of the results.
+==================== */
+
+let allResults;
+
+/* ====================
+Part 4: Add an event listener to the button in problem 4 that changes the
+button's own text to "I'm Clicked!"
+==================== */
+
+let imClickedButton;
+if (imClickedButton) {
+  imClickedButton.addEventListener('click', () => {});
+}
+
+/* ====================
+Part 5: Add an event listener to the button in problem 5 that creates a new span
+to the spanContainer. The span should contain a single number representing how
+many times you have clicked the button. For example, if it is your 3rd time
+clicking the button, you should add:
+
+  <span>3</span>
+
+HINT: You may need some global state for this problem.
+==================== */
+
+let spanContainer = document.querySelector('#span-container');
+spanContainer.appendChild(htmlToElement('<span>0</span>'));
+
+let addSpanButton;
+if (addSpanButton) {
+  addSpanButton.addEventListener('click', () => {});
+}
+
+/* =====================
+
+Results (all should report success)
+YOU NEED NOT (AND SHOULD NOT) EDIT BELOW THIS LINE.
+
+===================== */
+
+function checkResults() {
+  // Part 1
+  try {
+    firstResult.textContent = 'Success!';
+  } catch (exc) {
+    console.log('Failed part 1:');
+    console.error(exc);
+  }
+
+  // Part 2
+  try {
+    if (secondAndThirdResults.length !== 2) {
+      console.log(`Failed part 2: Variable should represent exactly 2 elements, not ${secondAndThirdResults.length}.`);
+    } else {
+      document.querySelector('#result-2').textContent = 'Success!';
+    }
+  } catch (exc) {
+    console.log('Failed part 2:');
+    console.error(exc);
+  }
+
+  // Part 3
+  try {
+    if (allResults.length < 3) {
+      console.log(`Failed part 3: Variable should represent ... elements, not ${secondAndThirdResults.length}.`);
+    } else {
+      document.querySelector('#result-3').textContent = 'Success!';
+    }
+  } catch (exc) {
+    console.log('Failed part 3:');
+    console.error(exc);
+  }
+
+  // Part 4
+  if (imClickedButton) {
+    imClickedButton.addEventListener('DOMNodeInserted', () => {
+      if (imClickedButton.textContent === 'I\'m Clicked!') {
+        document.querySelector('#result-4').textContent = 'Success!';
+      }
+    });
+  }
+
+  // Part 5
+  spanContainer.addEventListener('DOMNodeInserted', () => {
+    if (spanContainer.children.length === 6
+        && spanContainer.lastChild.textContent === '5') {
+      document.querySelector('#result-5').textContent = 'Success!';
+    }
+  });
+}
+
+checkResults();
