4c2c97e7cdb2c695663604a48d5e7a5db2380ebc
Jonathon Sun <77643405+Jonathonsun03@users.noreply.github.com>
2022-10-11 20:08:20 -0400

    I linted a bit

--- exercises/part1-selectors-and-events/index.js
+++ exercises/part1-selectors-and-events/index.js
@@ -18,7 +18,6 @@ first test result.
 
 let firstResult = document.getElementById('result-1');
 
-
 /* ====================
 Parts 2: Set the variable below equal to a collection of the paragraph
 elements representing the 2nd and 3rd results.
