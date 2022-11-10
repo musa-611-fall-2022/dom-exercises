6c87a15d260592e7579b60a2c755eb907873bc7e
sim-py <98434506+sim-py@users.noreply.github.com>
2022-11-09 21:08:22 -0500

    Update1

--- exercises/part1-selectors-and-events/index.js
+++ exercises/part1-selectors-and-events/index.js
@@ -16,30 +16,34 @@ Part 1: Set the variable below equal to the paragraph element representing the
 first test result.
 ==================== */
 
-let firstResult;
+let firstResult = document.querySelector('#result-1');
 
 /* ====================
 Parts 2: Set the variable below equal to a collection of the paragraph
 elements representing the 2nd and 3rd results.
 ==================== */
 
-let secondAndThirdResults;
+let secondAndThirdResults = document.querySelectorAll('.result-2-3')
+for (let i=0; i < secondAndThirdResults.length; i++);
 
 /* ====================
 Parts 3: Set the variable below equal to a collection of the paragraph
 elements representing the all of the results.
 ==================== */
 
-let allResults;
+let allResults = document.querySelectorAll('.result')
+for (let i=0; i < secondAndThirdResults.length; i++);
 
 /* ====================
 Part 4: Add an event listener to the button in problem 4 that changes the
 button's own text to "I'm Clicked!"
 ==================== */
 
-let imClickedButton;
+let imClickedButton=document.getElementById("im-clicked-button");
 if (imClickedButton) {
-  imClickedButton.addEventListener('click', () => {});
+  imClickedButton.addEventListener('click', () => {
+    imClickedButton.textContent="I'm Clicked!";
+  });
 }
 
 /* ====================
@@ -56,10 +60,16 @@ HINT: You may need some global state for this problem.
 let spanContainer = document.querySelector('#span-container');
 spanContainer.appendChild(htmlToElement('<span>0</span>'));
 
-let addSpanButton;
-if (addSpanButton) {
-  addSpanButton.addEventListener('click', () => {});
+let addSpanButton = document.getElementById("add-span-button");
+let n = 0;
+if (addSpanButton) { 
+  addSpanButton.addEventListener('click', () => {
+    n=n+1;
+    spanContainer.appendChild(htmlToElement("<span>"+n+"</span>"));
+  });
 }
+
+window.spanContainer=spanContainer;
 
 /* =====================
 
